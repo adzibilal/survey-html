84959106872d07789cbfa04b7c1fa9d58c3b7450
adzibilal <adzibilal02@gmail.com>
2024-05-12 11:19:59 +0700

    feat(reason): adding reason text area while value <= 3

--- assets/index.js
+++ assets/index.js
@@ -7,26 +7,23 @@ radioButtons.forEach((radioButton) => {
     });
 });
 
-
-// Ambil semua elemen input radio dalam kelas "rating-survey"
 const radioButtonsSurvey = document.querySelectorAll('.rating-survey input[type="radio"]');
+const questions = document.querySelectorAll('.question');
 
-// Loop melalui setiap elemen input radio
 radioButtonsSurvey.forEach(radioButton => {
-    // Tambahkan event listener untuk setiap elemen input radio
     radioButton.addEventListener('change', function() {
-        // Dapatkan elemen label terkait dengan input radio yang dipilih
         const label = this.parentElement;
-
-        // Dapatkan semua elemen label dalam kelas "rating-survey" yang terkait dengan input radio yang dipilih
         const labels = this.closest('.rating-survey').querySelectorAll('label');
-
-        // Loop melalui setiap elemen label dan hapus kelas "selected"
         labels.forEach(label => {
             label.classList.remove('selected');
         });
-
-        // Tambahkan kelas "selected" ke label yang terkait dengan input radio yang dipilih
         label.classList.add('selected');
+        const value = parseInt(this.value);
+        const textArea = this.closest('.question').querySelector('.reason');
+        if (value <= 3) {
+            textArea.style.display = 'block';
+        } else {
+            textArea.style.display = 'none';
+        }
     });
 });
